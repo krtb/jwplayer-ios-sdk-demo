bb3f7d61c3624af38c96715301e3c0ca4adfbea3
Kurt Bauer <kbauer@KBauer-MBP-13.home>
2020-05-20 16:57:38 -0400

    modify project to use Storyboard

---
+++ jwplayer_ios_sdk_demo/jwplayer_ios_sdk_demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jwplayer_ios_sdk_demo
+//
+//  Created by Kurt Bauer on 5/20/20.
+//  Copyright © 2020 JWPlayer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
